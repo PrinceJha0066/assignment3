5ae6d4056ddf46de226e519a2758b6594bfda829
PrinceJha0066 <137476066+PrinceJha0066@users.noreply.github.com>
2023-08-03 20:02:11 +0530

    Update assign3.py

--- assign3.py
+++ assign3.py
@@ -1,15 +1,29 @@
+print("Select an operation to perform:")
+print("1. Addition")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Division")
 
-select=int(input("select operations from 1,2,3,4:"))
-num1=int(input("enter first number"))
-num2=int(input("enter second number"))
+op = int(input("Select your operation: "))
 
-if select==1:
-    print(num1,"+",num2,"is" ,num1+num2)
-elif select ==2:
-    print(num1,"-",num2,"is" ,num1-num2)
-elif select ==3:
-    print(num1,"*",num2,"is" ,num1*num2)
-elif select==4:
-    print(num1,"/",num2,"is" ,num1/num2)
+if op == 1:
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    print("Result of adding two numbers is:", num1 + num2)
+elif op == 2:
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    print("Result of subtracting two numbers is:", num1 - num2)
+elif op == 3:
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    print("Result of multiplying two numbers is:", num1 * num2)
+elif op == 4:
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    if num2 != 0:
+        print("Result of dividing two numbers is:", num1 / num2)
+    else:
+        print("Cannot divide by zero!")
 else:
-    print("invalid choice")
+    print("Invalid operation")
